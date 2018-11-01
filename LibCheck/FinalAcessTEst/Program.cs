3462fba62226c71521d7e2aa0931cf77e7e1c3de
LOKESHGORLE <43067941+LOKESHGORLE@users.noreply.github.com>
2018-11-01 10:27:45 +0530

    initial check on console

---
+++ LibCheck/FinalAcessTEst/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FinalAcessTEst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DirectoryPath = @"C:\Users\lokesh.gorle\Desktop\SPFx PPT";
+
+            List<String> ab= DirSearch(DirectoryPath);
+            foreach(string i in ab)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("--------------with second one");
+            ab = GetAllFiles(DirectoryPath);
+            foreach (string i in ab)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+        public static List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+
+            return files;
+        }
+        public static List<String> GetAllFiles(String directory)
+        {
+            return Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).ToList();
+        }
+    }
+}
